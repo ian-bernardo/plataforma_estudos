fd2b05852ad8cd747ab136ad0b72f79aa0375335
Ian Bernardo <ianyurixv@gmail.com>
2026-02-18 13:47:57 -0300

    feat: redirect root para /painel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-black text-white p-10">
-      <h1 className="text-3xl font-bold">
-        Plataforma de Estudos
-      </h1>
-    </main>
-  );
+  redirect("/painel");
 }
+
 
